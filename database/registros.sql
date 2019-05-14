05f182d0d619a78a6106b723c61ed3d09d3654f0
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-05-13 19:53:45 -0500

    Iniciando repositorio tienda-camisetas

---
+++ database/registros.sql
@@ -0,0 +1,8 @@
+USE tienda_master;
+
+INSERT INTO usuarios VALUES(NULL, 'Admin', 'Admin', 'admin@admin.com', 'contraseña', 'admin', null);
+
+INSERT INTO categorias VALUES(null, 'Manga corta');
+INSERT INTO categorias VALUES(null, 'Tirantes');
+INSERT INTO categorias VALUES(null, 'Manga larga');
+INSERT INTO categorias VALUES(null, 'Sudaderas');
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="assets/css/normalize.css">
+        <link rel="stylesheet" href="assets/css/styles.css">
+        <title>Tienda de Camisetas</title>
+    </head>
+    <body>
+        <!--	CABECERA	-->
+           <header id="header">
+               <div id="logo">
+                   <img src="assets/img/camiseta.png" alt="Camiseta Logo">
+                   <a href="index.php">Tienda de camisetas</a>
+               </div>
+           </header>
+        <!--	MENU	-->
+        
+        <!--	BARRA LATERAL	-->
+            
+        <!--	CONTENIDO CENTRAL	-->
+        
+        <!--	PIE DE PÁGINA	-->
+        
+    </body>
+</html>
