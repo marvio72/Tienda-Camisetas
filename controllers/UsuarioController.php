fe4d2e7963cb8ca32fde429f1c32b3a2d70c3461
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-05-16 17:06:14 -0500

    Formulario de registro de usuarios

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -5,4 +5,14 @@ class UsuarioController{
     public function index(){
         echo "Controlador Usuarios, Acción index";
     }
+
+    public function registro(){
+        require_once 'views/usuario/registro.phtml';
+    }
+
+    public function save(){
+        if (isset($_POST)) {
+            var_dump($_POST);
+        }
+    }
 } 
