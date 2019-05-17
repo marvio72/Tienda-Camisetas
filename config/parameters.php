88aa21be70ae8e29c72511f2eaa07e8f58f49661
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-05-16 21:28:40 -0500

    Rutas amigables (htaccess)

---
+++ config/parameters.php
@@ -0,0 +1,5 @@
+<?php 
+
+define ('BASE_URL', 'http://localhost/master-php/tienda-camisetas/');
+define ('CONTROLLER_DEFAULT', 'productoController');
+define ('ACTION_DEFAULT', 'index');
--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php 
 
 require_once 'autoload.php';
+require_once 'config/parameters.php';
 require_once 'views/layout/header.phtml';
 require_once 'views/layout/sidebar.phtml';
 
+function show_error(){
+    $error = new ErrorController();
+    $error->index();
+}
 
 if (isset($_GET['controller'])) {
     $nombre_controlador = $_GET['controller'].'Controller';
+}elseif (!isset($_GET['controller']) && !isset($_GET['action'])) {
+    $nombre_controlador = CONTROLLER_DEFAULT;
 }else{
-    echo "La pagina que buscas no existe1";
+    show_error();
     exit();
 }
 
@@ -18,11 +25,15 @@ if (class_exists($nombre_controlador)) {
     if (isset($_GET['action']) && method_exists($controlador, $_GET['action'])) {
         $action = $_GET['action'];
         $controlador->$action();
-    }else{
-        echo "La pagina que buscas no existe2";
+    }elseif (!isset($_GET['controller']) && !isset($_GET['action'])) {
+        $action_default = ACTION_DEFAULT;
+        $controlador->$action_default();
+    }
+    else{
+        show_error();
     }
 }else{
-    echo "La pagina que buscas no existe3";
+    show_error();
 }
 
 require_once 'views/layout/footer.phtml';
