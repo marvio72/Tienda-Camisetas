9502477dd5440a101692c09ec6cddfe56183269d
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-05-21 15:10:50 -0500

    Gestión de Productos

--- controllers/ProductoController.php
+++ controllers/ProductoController.php
@@ -1,9 +1,18 @@
 <?php
-
+require_once 'models/producto.php';
 class ProductoController{
 
     public function index(){
         // echo "Controlador Productos, Acción index";
         require_once 'views/producto/destacados.phtml';
+    }
+
+    public function gestion(){
+        Utils::isAdmin();
+
+        $producto = new Producto();
+        $productos = $producto->getAll();
+
+        require_once 'views/producto/gestion.phtml';
     }
 }
---
+++ models/producto.php
@@ -0,0 +1,125 @@
+<?php 
+
+class Producto{
+    private $id;
+    private $categoria_id;
+    private $nombre;
+    private $descripcion;
+    private $precio;
+    private $stock;
+    private $oferta;
+    private $fecha;
+    private $imagen;
+    public  $db;
+
+    public function __construct(){
+        $this->db = Database::connect();
+    }
+
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getCategoria_id()
+    {
+        return $this->categoria_id;
+    }
+
+    public function setCategoria_id($categoria_id)
+    {
+        $this->categoria_id = $categoria_id;
+        return $this;
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+        return $this;
+    }
+
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+        return $this;
+    }
+
+    public function getPrecio()
+    {
+        return $this->precio;
+    }
+
+    public function setPrecio($precio)
+    {
+        $this->precio = $precio;
+        return $this;
+    }
+
+    public function getStock()
+    {
+        return $this->stock;
+    }
+
+    public function setStock($stock)
+    {
+        $this->stock = $stock;
+        return $this;
+    }
+
+    public function getOferta()
+    {
+        return $this->oferta;
+    }
+
+    public function setOferta($oferta)
+    {
+        $this->oferta = $oferta;
+        return $this;
+    }
+
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+
+    public function setFecha($fecha)
+    {
+        $this->fecha = $fecha;
+        return $this;
+    }
+
+    public function getImagen()
+    {
+        return $this->imagen;
+    }
+
+    public function setImagen($imagen)
+    {
+        $this->imagen = $imagen;
+        return $this;
+    }
+
+    public function getAll(){
+        $productos = $this->db->query("SELECT * FROM productos ORDER BY id DESC");
+        return $productos;
+    }
+
+}
