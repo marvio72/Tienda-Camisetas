854349f16ab460166db71efd75102d47b27bc9df
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-05-26 18:38:55 -0500

    Carrito de la compra (tienda virtual)

--- helpers/utils.php
+++ helpers/utils.php
@@ -82,5 +82,22 @@ class Utils{
         }
         return $resultado;
     }
+
+    public static function statsCarrito(){
+        $stats = array (
+            'count' => 0,
+            'total' => 0
+        );
+
+        if (isset($_SESSION['carrito'])) {
+            $stats['count'] = count($_SESSION['carrito']);
+            
+            foreach ($_SESSION['carrito'] as $producto ) {
+                $stats['total'] += $producto['precio']*$producto['unidades'];
+            }
+        }
+
+        return $stats;
+    }
         
 }
